7986018e49f26db11fa3714a2a90bd7d94a51f91
Tim Flucker <tflucker@gwmail.gwu.edu>
2023-05-08 15:42:30 -0400

    added initial schema and data for application

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
 -- DROP TABLES
-DROP TABLE IF EXISTS Pizza;
-DROP TABLE IF EXISTS Toppings;
-DROP TABLE IF EXISTS PizzaToppings;
-DROP TABLE IF EXISTS CustomerInfo;
-DROP TABLE IF EXISTS PizzaOrder;
+DROP TABLE IF EXISTS Pizza CASCADE;
+DROP TABLE IF EXISTS Toppings CASCADE;
+DROP TABLE IF EXISTS PizzaToppings CASCADE;
+DROP TABLE IF EXISTS Customer CASCADE;
+DROP TABLE IF EXISTS Pizza_Order CASCADE;
 
 
 -- Create all tables with constraints
 CREATE TABLE Pizza (
-  Pizza_ID 		SERIAL NOT NULL,
+  Pizza_ID 			SERIAL NOT NULL,
   Name 				VARCHAR(128) 	NOT NULL,
   Description 		VARCHAR(255) 	NOT NULL,
   Cost 				DECIMAL(5,2) 	NOT NULL,
@@ -23,41 +23,41 @@ CREATE TABLE Toppings (
   PRIMARY KEY (Toppings_ID)
 );
 
-CREATE TABLE Pizza_Toppings (
+/* CREATE TABLE Pizza_Toppings (
   Pizza_Toppings_ID 	SERIAL 	NOT NULL,
   Pizza_ID 			BIGINT 	NOT NULL,
   Toppings_ID 		BIGINT 	NOT NULL,
   PRIMARY KEY (Pizza_Toppings_ID),
   FOREIGN KEY(Pizza_ID) REFERENCES Pizza,
   FOREIGN KEY (Toppings_ID) REFERENCES Toppings
-);
+); */
 
-CREATE TABLE Customer_Info (
-  Customer_Info_ID 		SERIAL 			NOT NULL,
+CREATE TABLE Customer (
+  Customer_ID	 		SERIAL 			NOT NULL,
   Name 					VARCHAR(128) 	NOT NULL,
   Phone 				VARCHAR(15) 	NOT NULL,
   Email 				VARCHAR(128) 	NOT NULL,
   Receive_Promotions 	BOOLEAN 		NOT NULL,
-  PRIMARY KEY (Customer_Info_ID)
+  PRIMARY KEY (Customer_ID)
 );
 
 
 CREATE TABLE Pizza_Order (
   Pizza_Order_ID 	SERIAL 			NOT NULL,
-  Customer_Info_ID 	BIGINT 			NOT NULL,
+  Customer_ID	 	BIGINT 			NOT NULL,
   Pizza_ID 			BIGINT 			NOT NULL,
   Order_Date 		DATE 			NOT NULL,
   PRIMARY KEY (Pizza_Order_ID),
   FOREIGN KEY(Pizza_ID) REFERENCES Pizza,
-  FOREIGN KEY (Customer_Info_ID) REFERENCES Customer_Info
+  FOREIGN KEY (Customer_ID) REFERENCES Customer
 );
 
 
 -- Create indexes for tables (foreign key constraints
-CREATE INDEX PIZZA_ORDER_CUST_INFO_IDX ON  Pizza_Order (Customer_Info_ID);
+CREATE INDEX PIZZA_ORDER_CUST_INFO_IDX ON  Pizza_Order (Customer_ID);
 CREATE INDEX PIZZA_ORDER_PIZZA_IDX ON  Pizza_Order (Pizza_ID);
-CREATE INDEX PIZZA_TOPPINGS_TOPPINGS_IDX ON  Pizza_Toppings (Toppings_ID);
-CREATE INDEX PIZZA_TOPPINGS_PIZZA_IDX ON  Pizza_Toppings (Pizza_ID);
+/* CREATE INDEX PIZZA_TOPPINGS_TOPPINGS_IDX ON  Pizza_Toppings (Toppings_ID);
+CREATE INDEX PIZZA_TOPPINGS_PIZZA_IDX ON  Pizza_Toppings (Pizza_ID); */
 
 -- indexes for fields used in queries
 CREATE INDEX PIZZA_NAME_IDX ON PIZZA (Name);
