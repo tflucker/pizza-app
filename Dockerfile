990fcac376345fd2b0ee99326467287306ea6c7f
Tim Flucker <tflucker@gwmail.gwu.edu>
2023-06-16 18:50:28 -0400

    adding dockerfile for containerization, readme updates

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM eclipse-temurin:17-jdk-alpine
+
+VOLUME /tmp
+
+COPY target/PizzaApp-0.0.1-SNAPSHOT.jar app.jar
+
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/timflucker/pizza/app/controller/PizzaController.java
+++ src/main/java/com/timflucker/pizza/app/controller/PizzaController.java
@@ -3,25 +3,18 @@ package com.timflucker.pizza.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.timflucker.pizza.app.dto.CustomerDTO;
 import com.timflucker.pizza.app.dto.CustomerOrderDTO;
-import com.timflucker.pizza.app.model.Customer;
 import com.timflucker.pizza.app.model.Pizza;
-import com.timflucker.pizza.app.service.CustomerService;
 import com.timflucker.pizza.app.service.PizzaService;
 
 import io.micrometer.common.util.StringUtils;
-import jakarta.validation.Valid;
 
 /**
  * 
